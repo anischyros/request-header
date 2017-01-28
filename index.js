575c59e46c5e67f012c61854fae3611165ebf75f
Bob Sutton <anischyros@gmail.com>
2017-01-27 16:45:59 -0800

    Fixed code that determines remote IP address

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ console.log("Listening to port 8080");
 
 function onRequest(request, response)
 {
-	var host = request.headers.host;
+	var ip = request.connection.remoteAddress;
 	var acceptLanguage = request.headers["accept-language"];
 	var userAgent = request.headers["user-agent"];
 
 	var obj = {};
-	obj.host = host.split(":")[0];
+	ip = ip.split(":");
+	obj.ip = ip[ip.length - 1];
 	obj.language = acceptLanguage.split(",")[0];
 	var start = userAgent.indexOf("(");
 	var stop = userAgent.indexOf(")");
 	obj.os = userAgent.substring(start + 1, stop);
-	
+
 	response.end(JSON.stringify(obj));
 }
